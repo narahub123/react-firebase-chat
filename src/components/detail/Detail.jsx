a64530188cf41c90d415f7e73bd6ac28daec5fbd
nara <parknara0213@gmail.com>
2024-05-01 21:09:28 +0900

    240501 chat detail component

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,74 @@ import React from "react";
 import "./detail.css";
 
 const Detail = () => {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum dolor sit amet</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/19155212/pexels-photo-19155212/free-photo-of-roof-on-a-yellow-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>Photo_2024_02</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/19155212/pexels-photo-19155212/free-photo-of-roof-on-a-yellow-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>Photo_2024_02</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/19155212/pexels-photo-19155212/free-photo-of-roof-on-a-yellow-building.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>Photo_2024_02</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
