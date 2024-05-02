75de313c2edaa5f93814191fb0bafba7ab62bd26
nara <parknara0213@gmail.com>
2024-05-02 11:05:32 +0900

    240502 track the login process

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 const App = () => {
   const user = false;
+
+  // track of login process
+  // listen authentication when we create a new account
+  // and when we log in and log out and send us the latest status
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+
+    // clean up function
+    return () => {
+      unSub();
+    };
+  }, []);
 
   return (
     <div className="container">
