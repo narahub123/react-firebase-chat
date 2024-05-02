8d4ebc039407d67fb3811213c708a747722bb6b5
nara <parknara0213@gmail.com>
2024-05-03 01:53:51 +0900

    240503 create a new chat in chat list

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,21 +1,104 @@
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { db } from "../../../../lib/firebase";
 import "./addUser.css";
 
-import React from "react";
+import React, { useState } from "react";
+
+import { useUserStore } from "../../../../lib/userStore";
 
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+
+  const { currentUser } = useUserStore();
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+
+    try {
+      const userRef = collection(db, "users");
+
+      const q = query(userRef, where("username", "==", username));
+
+      const querySnapShot = await getDocs(q);
+
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data());
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAdd = async () => {
+    const chatRef = collection(db, "chats");
+    const userchatRef = collection(db, "userchats");
+
+    try {
+      // create collection reference
+      const newChatRef = doc(chatRef);
+
+      // add doc
+      await setDoc(newChatRef, {
+        createdAt: serverTimestamp(),
+        messages: [],
+      });
+
+      // get chatId
+      console.log(newChatRef.id);
+
+      // update user chat using chat id
+      await updateDoc(doc(userchatRef, user.id), {
+        // push any item inside the array
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage: "",
+          receiverId: currentUser.id,
+          updatedAt: Date.now(),
+        }),
+      });
+
+      // update currentUser chat using chat id
+      await updateDoc(doc(userchatRef, currentUser.id), {
+        // push any item inside the array
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage: "",
+          receiverId: user.id,
+          updatedAt: Date.now(),
+        }),
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="addUser">
-      <form>
+      <form onSubmit={handleSearch}>
         <input type="text" placeholder="Username" name="username" />
         <button>Search</button>
       </form>
-      <div className="user">
-        <div className="detail">
-          <img src="./avatar.png" alt="" />
-          <span>Jane Doe</span>
+      {user && (
+        <div className="user">
+          <div className="detail">
+            <img src={user.avatar || "./avatar.png"} alt="" />
+            <span>{user.username}</span>
+          </div>
+          <button onClick={handleAdd}>Add User</button>
         </div>
-        <button>Add User</button>
-      </div>
+      )}
     </div>
   );
 };
